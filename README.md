41a7d7a11efc63ae65d4479dac1c9b6fc02b7ca8
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-05-22 07:00:46 -0400

    Names changes, license, cleaning up code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# HCHRC
-
+# HCARC
 We are a youth community composed of avid ham radio operators. We do ham radio at Hack Club events, like at Outernet. Join us in the Hack Club Slack!
+
+# License
+Ham Club is open source and licensed under the [MIT License](https://github.com/crnicholson/hcarc/LICENSE.md). Fork, remix, and make it your own! Pull requests and other contributions are greatly appreciated.
--- app/page.tsx
+++ app/page.tsx
@@ -21,24 +21,8 @@ export default function Home() {
               }}
             ></div>
           ))}
-          {[...Array(12)].map((_, index) => (
-            <div
-              key={index}
-              className="absolute animate-radio-signal"
-              style={{
-                width: '5px',
-                height: '${Math.random() * 100}px',
-                backgroundColor: 'rgba(139, 69, 19, 0.5)',
-                animationDelay: `${index * 0.3}s`,
-                left: `${Math.random() * 100}%`,
-                top: `${Math.random() * 100}%`,
-                transformOrigin: 'top',
-                animationDuration: `${Math.random() * 2 + 1}s`,
-              }}
-            ></div>
-          ))}
         </div>
-        <header className="bg-[rgb(56,34,15)] shadow-xl relative z-10">
+        <header className="bg-[rgb(56,34,15)] shadow-2xl relative z-10">
           <a className="banner mx-auto ml-2 z-50" href="https://hackclub.com/">
             <img
               src="https://contribute.hackclub.com/images/flag-orpheus-top.png"
@@ -48,22 +32,22 @@ export default function Home() {
           <div className="flex w-full h-20 items-center">
             <div className="w-[15%] text-center"></div>
             <div className="w-[70%] text-center font-[blox] text-5xl text-[rgb(250,240,230)]">
-              <a href="#">HCHRC</a>
+              <a href="#">HCARC</a>
             </div>
             <div className="w-[15%] text-center md:visible invisible font-bold text-[rgb(250,240,230)] underline decoration-dotted text-xl hover:text-white">
-              <a href="https://hackclub.com/slack/">Get started</a>
+              <a href="https://hackclub.com/slack/?event=Hack%20Club%20Amateur%20Radio%20Club">Get started</a>
             </div>
           </div>
         </header>
         <ImageSlider />
         <main className="mx-auto text-left w-[50%] pt-10 text-[rgb(56,34,15)] mb-20 relative z-10">
           <div className="border-[5px] rounded-2xl border-dotted border-[rgb(56,34,15)] p-10 bg-white relative z-10">
-            <h1 className="text-3xl font-bold mb-4">Welcome to the Hack Club Ham Radio Club!</h1>
+            <h1 className="text-3xl font-bold mb-4">Welcome to the Hack Club Amateur Radio Club!</h1>
             <p className="text-lg">Join us monthly in the <code>#hamradio</code> channel of the Hack Club Slack. Experience ham radio at Hack Club events, like Outernet.</p>
             <br></br>
             <p className="text-lg">Stay tuned for exciting updates and activities!</p>
             <br></br>
-            <a href="https://hackclub.com/slack/" className="text-lg font-bold hover:underline inline-block">Get started.</a>
+            <a href="https://hackclub.com/slack/?event=Hack%20Club%20Amateur%20Radio%20Club" className="text-lg font-bold hover:underline inline-block">Get started.</a>
           </div>
         </main>
       </div>
